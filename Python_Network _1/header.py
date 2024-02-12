fff34a3085c824147f610017af3bbd05bde9bb7b
Zeberio <morandezeb@gmail.com>
2024-02-12 17:40:00 +0300

    displays the value of the X-Request-Id variable

---
+++ Python_Network _1/header.py
@@ -0,0 +1,10 @@
+'''#!/usr/bin/python3'''
+'''displays the value of the X-Request-Id variable found in the header of the response.'''
+
+import sys
+import urllib.request
+
+if __name__ == '__main__':
+    req = urllib.request.Request(sys.argv[1])
+    with urllib.request.urlopen(reg) as response:
+        print(dict(response.headers).get('X-Request-Id'))
