a9534d79c225f7bbe641a1c190f5229dcafa49da
Zeberio <morandezeb@gmail.com>
2024-02-12 17:26:27 +0300

    update solution

--- Python_Network _1/status.py
+++ Python_Network _1/status.py
@@ -8,5 +8,5 @@ with urllib.request.urlopen(req) as response:
     body = response.read()
     print('Body response:$')
     print('\t- type: {}$'.format(type(body)))
-    print('\t- content: {}'.format(body))
-    print('\t- utf-8: {}'.format(body.decode('utf-8')))
+    print('\t- content: {}$'.format(body))
+    print('\t- utf-8: {}$'.format(body.decode('utf-8')))
