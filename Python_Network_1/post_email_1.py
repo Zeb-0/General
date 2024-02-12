fe0dd76da16cc522123ec855b29f94ab326ff80c
Zeberio <morandezeb@gmail.com>
2024-02-12 19:28:54 +0300

    Post email using - requests instead

---
+++ Python_Network_1/post_email_1.py
@@ -0,0 +1,11 @@
+'''#!/usr/bin/python'''
+'''sends a POST request to the passed URL with the email as a parameter'''
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    value = {'email':sys. argv[2]}
+    req = requests.post(url, data=value)
+    print(req.text)
