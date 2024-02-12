cbb93ac46b05fe09b207428b3d6fb41e65b947a7
Zeberio <morandezeb@gmail.com>
2024-02-12 19:18:20 +0300

    some adjustments

--- Python_Network_1/fetch_status.py
+++ Python_Network_1/fetch_status.py
@@ -2,7 +2,9 @@
 '''fetch https://alx-intranet.hbtn.io/status'''
 import requests
 
-req = requests.get('https://alx-intranet.hbtn.io/status')
-print('Body response:&')
-print('\t- type: {}$'.format(type(req.text)))
-print('\t-content: {} OK$'.format(req.text))
+
+if __name__ == '__main__':
+    req = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:&')
+    print('\t- type: {}$'.format(type(req.text)))
+    print('\t-content: {} OK$'.format(req.text))
