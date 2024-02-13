bb219871ca741cc6ee25afda496b1a79bb9a264b
Zeberio <morandezeb@gmail.com>
2024-02-13 20:17:14 +0300

    some corrections

--- Python_Network_1/github.py
+++ Python_Network_1/github.py
@@ -7,5 +7,5 @@ from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    resp = requests.get('https://github.com/user', auth=auth)
+    resp = requests.get('https://api.github.com/user', auth=auth)
     print(resp.json().get('id'))
