aca4b782b4f55f3d8cbf5916d85bf185b1925cb3
Zeberio <morandezeb@gmail.com>
2024-02-26 11:56:53 +0300

    some glass shades class

---
+++ some_class.py
@@ -0,0 +1,11 @@
+class Glasses:
+    def printShadeIndex(self):
+        print('low')
+
+class Sunglasses(Glasses):
+    def printShadeIndex(self):
+        print('high')
+
+
+glasses = Glasses()
+glasses.printShadeIndex()
